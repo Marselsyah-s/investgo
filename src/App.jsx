ed065daf5321d79d51c273b849eaddfdfea0254a
Noven24 <dianwannovennf@gmail.com>
2026-05-01 13:58:48 +0700

    feat: Gamified Dashboard layout with Learning Path and Leaderboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import DashboardLayout from './components/DashboardLayout'
 import './index.css'
 
 function App() {
@@ -7,6 +9,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        {/* Dashboard routes wrapped in Layout */}
+        <Route path="/dashboard" element={<DashboardLayout><Dashboard /></DashboardLayout>} />
+        {/* Redirect unknown routes for now */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
